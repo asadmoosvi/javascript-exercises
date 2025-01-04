373a238b2fce79091e5774f8bf738762b7e29b07
Asad Moosvi <moosvi.asad@gmail.com>
2025-01-04 19:35:36 +0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // first we remove punctuation
+    let result = str.replace(/[^a-zA-Z0-9]/g, '');
+    result = result.toLowerCase();
+    const reversed_result = result.split('').reverse().join('');
+    return result === reversed_result;
 };
 
 // Do not edit below this line
