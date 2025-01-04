98085ae6946417443b82a0e484337bccab1c0d3b
Asad Moosvi <moosvi.asad@gmail.com>
2025-01-04 23:12:37 +0500

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = Number(n);
+    let i = 0;
+    let j = 1;
+    let temp;
 
+    if (n < 0) {
+        return 'OOPS';
+    } else if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        let iterations = n - 1;
+        for (let k = 0; k < iterations; ++k) {
+            temp = i + j;
+            i = j;
+            j = temp;
+        }
+        return j;
+    }
 };
 
 // Do not edit below this line
