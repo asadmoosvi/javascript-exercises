dd8476b43216d829c765fd84efac16427e60042f
Asad Moosvi <moosvi.asad@gmail.com>
2025-01-04 23:32:26 +0500

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    people.sort((a, b) => {
+        let ageA, ageB;
+        if (!a.yearOfDeath) {
+            ageA = new Date().getFullYear() - a.yearOfBirth;
+        } else {
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (!b.yearOfDeath) {
+            ageB = new Date().getFullYear() - b.yearOfBirth;
+        } else {
+            ageB = b.yearOfDeath - b.yearOfBirth;
+        }
+        return ageA > ageB ? -1 : 1;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
